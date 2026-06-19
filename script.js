e52739873171c39cbb441233c05713a06dbbb0be
thuynttussh-ops <thuyntt.ussh@gmail.com>
2026-06-19 16:40:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let allProperties = [];
 const sheetURL =
 "https://docs.google.com/spreadsheets/d/e/2PACX-1vSv3GJHvqIiznp0DKYlJBvvEeWnZDbsmkHIAZWVrYuwVc_bBq9TLZyLCvtRuE4TylHm-teSIjG4nDvC/pub?gid=0&single=true&output=csv";
 
@@ -7,15 +8,74 @@ Papa.parse(sheetURL, {
 
     complete: function(results){
 
-        const data = results.data;
+const data = results.data.filter(item => item.PropertyName);
 
-        console.log(data[0]);
+  allProperties = data;
 
-        displayProperties(data);
+createCityDropdown();
+
+displayProperties(data);
 
     }
 
 });
+function createCityDropdown(){
+
+    const citySelect = document.getElementById("cityFilter");
+
+    citySelect.innerHTML =
+    '<option value="">Chọn thành phố</option>';
+
+    const cities = [...new Set(
+        allProperties.map(item => item.City)
+    )];
+
+    cities.sort();
+
+    cities.forEach(city => {
+
+        citySelect.innerHTML +=
+        `<option value="${city}">${city}</option>`;
+
+    });
+
+}
+function createWardDropdown(city){
+
+    const wardSelect =
+    document.getElementById("wardFilter");
+
+    wardSelect.innerHTML =
+    '<option value="">Chọn quận</option>';
+
+    if(city==="") return;
+
+    const wards = [...new Set(
+
+        allProperties
+        .filter(item => item.City === city)
+        .map(item => item.Ward)
+
+    )];
+
+    wards.sort();
+
+    wards.forEach(ward=>{
+
+        wardSelect.innerHTML +=
+        `<option value="${ward}">${ward}</option>`;
+
+    });
+
+}
+document
+.getElementById("cityFilter")
+.addEventListener("change",function(){
+
+    createWardDropdown(this.value);
+
+});
+
 function displayProperties(properties){
 
     const container = document.getElementById("property-list");
